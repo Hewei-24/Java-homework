be50454dc7ec1d39ccf0d686fdc3c486e9bf2b9f
cnm24 <1470655157@qq.com>
2025-04-14 20:35:40 +0800

    Add files via upload

---
+++ interface/bookexample/example9_3/Exaple9_3.java
@@ -0,0 +1,9 @@
+package bookexample.example9_3;
+
+public class Exaple9_3 {
+    public static void main(String[] args) {
+        ComponentInWindow win = new ComponentInWindow();
+        win.setBounds(100, 100, 450, 300);
+        win.setTitle("常用组件");
+    }
+}
