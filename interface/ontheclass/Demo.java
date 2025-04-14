4f85ce087ae99eb4b41f7221d7a0276b3e0184a5
cnm24 <1470655157@qq.com>
2025-04-14 20:29:17 +0800

    Add files via upload

---
+++ interface/ontheclass/Demo.java
@@ -0,0 +1,12 @@
+package ontheclass;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        MyFrm_FlowLayout frm=new MyFrm_FlowLayout();
+        // MyFrm_BorderLayout frm=new MyFrm_BorderLayout();
+
+    }
+
+}
