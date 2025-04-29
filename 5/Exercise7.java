3b4a1e23f5d247049d7ca5e67da1693fae6cc6c9
Sbeth <1470655157@qq.com>
2025-04-29 22:19:46 +0800

    Update Exercise7.java

--- 5/Exercise7.java
+++ 5/Exercise7.java
@@ -2,32 +2,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Exercise7 {
+public class Exercise7_20241576_5_7 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("认证方式选择");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        JPanel leftPanel = new JPanel();
-        leftPanel.setBorder(BorderFactory.createTitledBorder("选择认证方式"));
-        ButtonGroup group = new ButtonGroup();
-        String[] authTypes = {"护照", "身份证", "社会保障卡", "驾驶证"};
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createTitledBorder("选择认证方式"));
 
-        for (String auth : authTypes) {
-            JRadioButton rb = new JRadioButton(auth);
-            rb.addActionListener(e -> {
-                if (rb.isSelected()) {
-                    ((JTextField)frame.getContentPane().getComponent(1))
-                            .setText("您选择的认证方式是: " + auth);
-                }
-            });
-            group.add(rb);
-            leftPanel.add(rb);
-        }
+        String[] authTypes = {"护照", "身份证", "社会保障卡", "驾驶证"};
+        JComboBox<String> comboBox = new JComboBox<>(authTypes);
 
         JTextField resultField = new JTextField(20);
         resultField.setEditable(false);
 
-        frame.add(leftPanel, BorderLayout.WEST);
+        comboBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String selected = (String) comboBox.getSelectedItem();
+                resultField.setText("您选择的认证方式是: " + selected);
+            }
+        });
+
+        panel.add(comboBox);
+        frame.add(panel, BorderLayout.WEST);
         frame.add(resultField, BorderLayout.EAST);
         frame.pack();
         frame.setVisible(true);
