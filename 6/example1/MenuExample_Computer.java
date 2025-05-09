8534a4a369e153bb93cc3113fa0f8db744a832cc
14706 <1470655157@qq.com>
2025-05-09 22:02:07 +0800

    0

---
+++ 6/example1/MenuExample_Computer.java
@@ -0,0 +1,10 @@
+package example1;
+
+import javax.swing.*;
+
+public class MenuExample_Computer {
+    public static void main(String[] args){
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        MyWindow win = new MyWindow("计算窗口");
+    }
+}
